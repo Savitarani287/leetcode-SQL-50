a1a1c9956f0c109b168f9160bd261860eb92ae4e
SAVITA RANI <savitaarani952@gmail.com>
2026-07-12 16:00:18 +0530

    Time: 326 ms (76.13%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,5 +1,2 @@
 # Write your MySQL query statement below
-select class
-from
-(select class,count(student) as cnt  from Courses  group by class)as new_table
-where cnt>=5;
+select class from Courses group by class having count(class) >= 5;
