b244f5b835d553fba8d3c55affe859dd413cd94b
SAVITA RANI <savitaarani952@gmail.com>
2026-07-12 15:08:15 +0530

    Time: 568 ms (99.51%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,11 +1,15 @@
 # Write your MySQL query statement below
-select  round(
-    sum(
-        if (earlier_order =earlier_pref_delivery_date,1,0))*100 / count(customer_id),2) as immediate_percentage
-from
-(select delivery_id,
-    customer_id ,
-    min(order_date) as earlier_order,
-   min(customer_pref_delivery_date) as earlier_pref_delivery_date
-from Delivery 
-group by customer_id ) as new_table
+select  
+    round(
+        sum(
+            if (earlier_order =earlier_pref_delivery_date,1,0))
+            *100 / count(customer_id), 
+            2) 
+            as immediate_percentage
+        from
+        (select delivery_id,
+        customer_id ,
+        min(order_date) as earlier_order,
+        min(customer_pref_delivery_date) as earlier_pref_delivery_date
+    from Delivery 
+    group by customer_id ) as new_table
