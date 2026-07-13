0b41fee9906b3572a52f995d632eef4308f82674
SAVITA RANI <savitaarani952@gmail.com>
2026-07-13 15:07:12 +0530

    Time: 896 ms (41.50%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,11 +1,27 @@
 # Write your MySQL query statement below
-select q1.person_name
-from Queue q1
-inner join 
-Queue q2 on q1.turn >= q2.turn
-group by q1.turn
-having sum(q2.Weight)<=1000
-order by q1.turn desc  limit  1;
+-- select q1.person_name
+-- from Queue q1
+-- inner join 
+-- Queue q2 on q1.turn >= q2.turn
+-- group by q1.turn
+-- having sum(q2.Weight)<=1000
+-- order by q1.turn desc  limit  1;
+
+# Write your MySQL query statement below
+
+SELECT person_name
+FROM Queue
+WHERE turn = (
+    SELECT max(turn)
+    FROM (
+        SELECT turn, person_id, person_name, weight, SUM(weight) OVER (ORDER BY turn) AS total_weight
+        FROM Queue
+    ) main
+    WHERE total_weight <= 1000 
+)
+
+
+
 
 
 
